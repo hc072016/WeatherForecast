3f02d37487f252616dae34bc97fbb929223c6e8e
Howie C <howie@air.local>
2019-12-17 21:01:33 -0500

    Initial Commit

---
+++ WeatherForecast/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeatherForecast
+//
+//  Created by Howie C on 12/17/19.
+//  Copyright © 2019 Howie C. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
